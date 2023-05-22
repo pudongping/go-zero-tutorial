8fec1c77a3849193ef4c055322917e360a3ae2ee
pudongping <276558492@qq.com>
2023-05-22 15:55:09 +0800

    http 拦截器（处理404、405、401情况）

--- common/httpRest/notAllowedHandler.go
+++ common/httpRest/notAllowedHandler.go
@@ -7,17 +7,15 @@ import (
 )
 
 func NotAllowedHandler() http.Handler {
-	return http.HandlerFunc(MethodNotAllowed)
-}
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
 
-func MethodNotAllowed(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-
-	resp := map[string]interface{}{
-		"code": http.StatusMethodNotAllowed,
-		"msg":  fmt.Sprintf("路由[%s]，当前不支持 %s 方法", r.URL.Path, r.Method),
-	}
-	bs, _ := json.Marshal(resp)
-	w.Write(bs)
+		resp := map[string]interface{}{
+			"code": http.StatusMethodNotAllowed,
+			"msg":  fmt.Sprintf("路由[%s]，当前不支持 %s 方法", r.URL.Path, r.Method),
+		}
+		bs, _ := json.Marshal(resp)
+		w.Write(bs)
+	})
 }
